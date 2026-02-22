9b031ab820dc55a7b7bd95e9ccbb380a58f2ccdb
suriyabalaji69 <your.sb5412@srmist.edu.in>
2026-02-22 21:02:31 +0530

    UC2 to Print OOPSBanner

---
+++ OOPSBannerApp2.java
@@ -0,0 +1,20 @@
+/**
+ * @author [SURIYA]
+ * @version 1.0
+ */
+public class OOPSBannerApp2 {
+
+    public static void main(String[] args) {
+        // Now we just call the method instead of writing all the print lines here
+        displayOopsBanner();
+    }
+
+    // This is the new function (method) we created to hold the banner logic
+    public static void displayOopsBanner() {
+        System.out.println(" ****   ****  ****  **** ");
+        System.out.println("*    * *    * *   * * ");
+        System.out.println("*    * *    * ****  **** ");
+        System.out.println("*    * *    * *        *");
+        System.out.println(" ****   ****  *     **** ");
+    }
+}
