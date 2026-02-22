c05b7d894d90a080aee38d37dc21c17b36928c86
suriyabalaji69 <your.sb5412@srmist.edu.in>
2026-02-22 20:37:23 +0530

    UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * * @author suriyabalaji69
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        // Use System.out.println("OOPS") to print to the console
+        System.out.println("OOPS");
+    }
+}
